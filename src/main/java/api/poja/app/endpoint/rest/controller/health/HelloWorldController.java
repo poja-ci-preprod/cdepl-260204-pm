287b3a799156f2a3307d0473a8f7fed895a8a6b9
yumii.saiko <hei.hajatiana@gmail.com>
2026-02-04 14:45:24 +0300

    chore: restore

--- src/main/java/api/poja/app/endpoint/rest/controller/health/HelloWorldController.java
+++ src/main/java/api/poja/app/endpoint/rest/controller/health/HelloWorldController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldController {
   @GetMapping("/hello-world")
   public String helloWorld() {
-    return "Hello world 1";
+    return "HHello world from Poja!";
   }
 }
